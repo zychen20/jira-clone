3a2348de933537aaed3a164bcdd2a683fc96fe3a
Z. Chen <69495364+zychen20@users.noreply.github.com>
2023-02-02 14:57:21 -0800

    feat: add project list
    Changes:
    - installed qs package
    - updated db.json with real data
    - configured CRA environment variables

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,37 @@
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import { useEffect, useState } from "react";
+import { cleanObject } from "utils";
+import * as qs from "qs";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+export const ProjectListScreen = () => {
+    const [param, setParam] = useState ({
+        name: '',
+        personId: ''
+    })
+    const [list, setList] = useState([])
+    const [users, setUsers] = useState([])
+
+
+    useEffect(() => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+            if (response.ok) {
+                setList(await response.json())
+            }
+        })
+    }, [param])
+
+    useEffect(() => {
+        fetch(`${apiUrl}/users`).then(async response => {
+            if (response.ok) {
+                setUsers(await response.json())
+            }
+        })
+    }, [])
+
+    return <div>
+        <SearchPanel users={users} param={param} setParam={setParam}></SearchPanel>
+        <List users={users} list={list}></List>
+    </div>
+}
