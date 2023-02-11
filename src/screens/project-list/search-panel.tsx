92ffeef4a2e36141cd0f00960773971462d83477
Z. Chen <69495364+zychen20@users.noreply.github.com>
2023-02-11 13:51:48 -0800

    chore: refactor from JS to TS

---
+++ src/screens/project-list/search-panel.tsx
@@ -0,0 +1,40 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  title: string;
+}
+
+interface SearchPanelProps {
+  users: User[];
+  param: {
+    name: string;
+    personId: string;
+  };
+  setParam: (param: SearchPanelProps["param"]) => void;
+}
+
+export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
+  return (
+    <form action="">
+      <input
+        type="text"
+        value={param.name}
+        onChange={(event) => setParam({ ...param, name: event.target.value })}
+      />
+      <select
+        value={param.personId}
+        onChange={(event) =>
+          setParam({ ...param, personId: event.target.value })
+        }
+      >
+        <option value={""}>负责人</option>
+        {users.map((user) => (
+          <option key={user.id} value={user.id}>
+            {user.name}
+          </option>
+        ))}
+      </select>
+    </form>
+  );
+};
