f8add04d102ccbc5ca764e0bc42dd9efbb536cbf
Z. Chen <69495364+zychen20@users.noreply.github.com>
2023-03-09 09:27:57 -0800

    feat: login form & scren

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import "./App.css";
-import { ProjectListScreen } from "screens/project-list";
+import React from 'react'
+import './App.css'
+import { ProjectListScreen } from 'screens/project-list'
+import { LoginScreen } from 'screens/login'
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      {/* <ProjectListScreen /> */}
+      <LoginScreen />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
