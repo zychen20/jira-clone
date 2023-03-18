8e5b37cf9b58e8a1cf14bc82a2495ae168df626a
Z. Chen <69495364+zychen20@users.noreply.github.com>
2023-03-18 14:23:15 -0700

    feat: useAuth hook

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,3 +1,4 @@
+import { useAuth } from "context/auth-context";
 import React, { FormEvent } from "react";
 import { cleanObject } from "utils";
 
@@ -18,18 +19,7 @@ import { cleanObject } from "utils";
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export const LoginScreen = () => {
-  const login = (param: { username: string; password: string }) => {
-    fetch(`${apiUrl}/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(param),
-    }).then(async (response) => {
-      if (response.ok) {
-      }
-    });
-  };
+  const { login, user } = useAuth();
 
   // HTMLFormElement extends Element
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
@@ -43,6 +33,7 @@ export const LoginScreen = () => {
 
   return (
     <form onSubmit={handleSubmit}>
+      {user ? <div>登录成功，用户名 {user?.name}</div> : null}
       <div>
         <label htmlFor="username">用户名</label>
         <input type="text" id={"username"} />
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 export interface User {
+  token: string;
   id: string;
   name: string;
   email: string;
