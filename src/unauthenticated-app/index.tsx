fe0f15be1137df1a01922d4d72db17d16989f0b1
Zhenyu Chen <69495364+zychen20@users.noreply.github.com>
2023-08-17 00:37:09 -0700

    style: apply antd UI components

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { useState } from "react";
 import { LoginScreen } from "./login";
 import { RegisterScreen } from "./register";
+import { Card } from "antd";
 
 export const UnauthenticatedApp = () => {
   const [isRegister, setIsRegister] = useState(false);
   return (
-    <div>
-      {isRegister ? <RegisterScreen /> : <LoginScreen />}
-      <button onClick={() => setIsRegister(!isRegister)}>
-        切换到{isRegister ? "登录" : "注册"}
-      </button>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Card>
+        {isRegister ? <RegisterScreen /> : <LoginScreen />}
+        <button onClick={() => setIsRegister(!isRegister)}>
+          切换到{isRegister ? "登录" : "注册"}
+        </button>
+      </Card>
     </div>
   );
 };
