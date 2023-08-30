3494258eaf7d7136465345bae535dd72dce9c780
Zhenyu Chen <69495364+zychen20@users.noreply.github.com>
2023-08-30 16:47:57 -0700

    style: implement custom Row component

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,20 +2,21 @@ import React from "react";
 import { ProjectListScreen } from "screens/project-list";
 import { useAuth } from "context/auth-context";
 import styled from "@emotion/styled";
+import { Row } from "components/lib";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <div>
-      <Header>
-        <LeftHeader>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
           <h3>Logo</h3>
           <h3>项目</h3>
           <h3>用户</h3>
-        </LeftHeader>
-        <RightHeader>
+        </HeaderLeft>
+        <HeaderRight>
           <button onClick={logout}>登出</button>
-        </RightHeader>
+        </HeaderRight>
       </Header>
       <Main>
         <ProjectListScreen />
@@ -24,20 +25,13 @@ export const AuthenticatedApp = () => {
   );
 };
 
-const Header = styled.header`
-  display: flex;
-  flex-direction: row;
+const Header = styled(Row)`
   height: 6rem;
-  align-items: center;
-  justify-content: space-between;
 `;
 
-const LeftHeader = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
+const HeaderLeft = styled(Row)``;
 
-const RightHeader = styled.div``;
+const HeaderRight = styled.div``;
 
 const Main = styled.main`
   height: calc(100vh - 6rem);
